82a3edcc13e24be54f894ee7e56a5f958f96edca
Carlos Iribar <carlosiribar@gmail.com>
2018-12-01 11:14:36 -0300

    add vue

--- public/app.js
+++ public/app.js
@@ -20,12 +20,10 @@
 const books = Vue.component('books', {
     data: function () {
         return {
-            message: 'Hello Vue!',
             books: []
         }
     },
     template:`<div>
-        {{ message }}
         <table>
         <tr>
         <th>Name</th>
@@ -35,13 +33,14 @@ const books = Vue.component('books', {
         <th>Actions</th>
         </tr>
         <tr v-for="row in books">
-            <td><a href=""> {{row['name']}} </a></td>
+            <td> <router-link to="/books">{{row['name']}}</router-link></td>
             <td>{{row['author']}}</td>
             <td>{{row['isbn']}}</td>
             <td>{{row['user']}}</td>
             <td><button onClick=''> Remove </button></td>
             <td><button onClick=''> Edit </button></td>
         </tr>
+        </table>
         </div>`,
     methods: {
         getBooks(resource) {
@@ -53,10 +52,43 @@ const books = Vue.component('books', {
     beforeMount(){
         this.getBooks()
     }
-  })
+})
+
+const books = Vue.component('book', {
+    props: ['id'],
+    data: function () {
+        return {
+            book: []
+        }
+    },
+    template:`<div>
+        <a href="/books.php"> Back to Book List </a>
+        <h1>Book Detail</h1>
+        <b>Name:</b>
+        {{$row['name']}}<br>
+        <b>Author:</b>
+        {{$row['author']}}<br>
+        <b>ISBN:</b> 
+        {{$row['isbn']}}<br>
+        <b>Owner:</b> 
+        {{$row['user']}}<br>
+        <img height='300px' width='200px' src="{{$row['cover']}}"/><br>
+        </div>`,
+    methods: {
+        getBook() {
+            this.$http.get('/book', {id: this.id}).then((response) => {
+                this.books = response.body
+            });
+        }
+    },
+    beforeMount(){
+        this.getBook()
+    }
+})
 
 const routes = [
     { path: '/', component: books },
+    { path: '/book/:id', component: books, props: true }
 ]
 
 const router = new VueRouter({
